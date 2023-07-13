a53cf5540c5bafdaa34e57fe1ddd592c9c904571
haroldZS <haroldzs1024@gmail.com>
2023-07-13 15:38:04 -0400

    Customer table and one-to-one relationship

---
+++ services/customer.service.js
@@ -0,0 +1,38 @@
+const boom = require('@hapi/boom');
+const { models } = require('../libs/sequelize');
+
+class CustomerService {
+  constructor() {}
+
+  async find() {
+    const rta = await models.Customer.findAll();
+    return rta;
+  }
+
+  async findOne(id) {
+    const user = await models.Customer.findByPk(id);
+    if (!user) {
+      throw boom.notFound('Customer nout found');
+    }
+    return user;
+  }
+
+  async create(data) {
+    const newCustomer = await models.Customer.create(data);
+    return data;
+  }
+
+  async update(id, changes) {
+    const model = await this.findOne(id);
+    const rta = await model.update(changes);
+    return rta;
+  }
+
+  async delete(id) {
+    const model = await this.findOne(id);
+    await model.destroy(id);
+    return { rta: true };
+  }
+}
+
+module.exports = CustomerService;
