0493ede2ffb237a3a32c26105355c7ddc6428c7c
haroldZS <haroldzs1024@gmail.com>
2023-07-13 17:34:49 -0400

    Bidirectional relationship by Customer to User

--- schemas/customer.schema.js
+++ schemas/customer.schema.js
@@ -1,16 +1,21 @@
 const Joi = require('joi');
 
 const id = Joi.number().integer();
-const name = Joi.string().min(3).max(5);
+const name = Joi.string().min(3).max(10);
 const lastName = Joi.string();
 const phone = Joi.string();
 const userId = Joi.number().integer();
+const email = Joi.string().email();
+const password = Joi.string().min(8);
 
 const createCustomerSchema = Joi.object({
   name: name.required(),
   lastName: lastName.required(),
   phone: phone.required(),
-  userId: userId.required(),
+  user: Joi.object({
+    email: email.required(),
+    password: password.required(),
+  }),
 });
 
 const updateCustomerSchema = Joi.object({
