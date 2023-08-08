b31300ae08aa1cc9eec4c07e0fc4e3e5b7ab95e2
haroldZS <haroldzs1024@gmail.com>
2023-08-08 17:54:14 -0400

    Auth service and sending an email to user

---
+++ services/auth.service.js
@@ -0,0 +1,64 @@
+const boom = require('@hapi/boom');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
+
+const { config } = require('./../config/config');
+
+const UserService = require('./user.service');
+const service = new UserService();
+
+class AuthService {
+  async getUser(email, password) {
+    const user = await service.findByEmail(email);
+    if (!user) {
+      throw boom.unauthorized();
+    }
+    const isMatched = await bcrypt.compare(password, user.password);
+    if (!isMatched) {
+      throw boom.unauthorized();
+    }
+    delete user.dataValues.password;
+    return user;
+  }
+
+  signToken(user) {
+    const payload = {
+      sub: user.id,
+      role: user.role,
+    };
+    const token = jwt.sign(payload, config.jwtSecret);
+    return {
+      user,
+      token,
+    };
+  }
+
+  async sendMail(email) {
+    const user = await service.findByEmail(email);
+    if (!user) {
+      throw boom.unauthorized();
+    }
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      secure: true,
+      port: 465,
+      auth: {
+        user: config.emailUser,
+        pass: config.emailPassword,
+      },
+    });
+
+    await transporter.sendMail({
+      from: config.emailUser,
+      to: `${user.email}`,
+      subject: 'New email',
+      text: 'Hello there!, from Node application',
+      html: '<b>Hello there!, from Node application</b>',
+    });
+
+    return { message: 'mail sent' };
+  }
+}
+
+module.exports = AuthService;
